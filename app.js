ec37859784add916cff17b86fc94df33155bd76a
starkduebusy <whitesolver@gmail.com>
2019-04-29 17:47:43 +0900

    delete test page

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var testRouter = require('./routes/test');
 
 var app = express();
 
@@ -27,7 +26,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/test', testRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
