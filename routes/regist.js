0ed8dec00015e10572f7d39f8a103a562f490652
starkduebusy <whitesolver@gmail.com>
2019-04-30 22:08:34 +0900

    회원가입 백엔드 개발중, 화면단 버그수정완료

--- routes/regist.js
+++ routes/regist.js
@@ -1,10 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var sqlManager = require('../manager/sqlManager');
 
 
 router.get('/', function(req, res, next) {
   res.render('regist');
 });
 
+var multer  = require('multer');
+var upload = multer();
+router.post('/', upload.array('profileIMG',1),function(req, res, next) {
+  sqlManager(function(err, con) {
+    var query = 'SELECT COUNT(*) AS isExist FROM DIET_MANAGER.USER WHERE EMAIL = ?';
+    con.query(query, req.body.email, function(err, result){
+      con.release();
+      if(err){
+        next(new Error('ERR006'));
+        return;
+      }
+
+      var resultParams = {
+        isSuccess : true
+      };
+     
+      res.send(resultParams);
+    });
+  });
+});
 
 module.exports = router;
