947691bce14f60135139cb9b73f7fedd9f7c7355
starkduebusy <whitesolver@gmail.com>
2019-05-03 20:59:35 +0900

    나의프로필 화면단 개발 완료

--- routes/profile.js
+++ routes/profile.js
@@ -7,9 +7,41 @@ router.get('/', function(req, res, next) {
   var params = {
     userName : (req.session == undefined)? null:req.session.userName,
     targetWeight : (req.session == undefined)? null:req.session.targetWeight,
-    profileIMG : (req.session == undefined)? null:req.session.profileIMG
+    profileIMG : (req.session == undefined)? null:req.session.profileIMG,
+    email : (req.session == undefined)? null:req.session.email,
+    birth : '',
+    weight : '',
+    height : '',
+    gender : ''
   };
-  res.render('profile', params);
+
+  if(req.session.session == undefined){
+    res.render('profile', params);
+    return;
+  }
+
+  sqlManager(function(err, con) {
+    var queryUser = 'SELECT * FROM USER WHERE EMAIL = ?;';
+    con.query(queryUser, req.session.email, function(err, result){
+      con.release();
+      if(err){
+        con.release();
+        next(new Error('ERR006|' + req.countryCode));
+        return;
+      }
+      
+      if(result.length != 0){
+        params.gender = result[0].GENDER; 
+        var birth = new Date(result[0].BIRTH_YMD);
+        birth.setDate(birth.getDate() +1);
+        params.birth = birth;
+        params.weight = result[0].WEIGHT;
+        params.height = result[0].HEIGHT;
+      }
+
+      res.render('profile', params);  
+    });
+  });
 });
 
 
