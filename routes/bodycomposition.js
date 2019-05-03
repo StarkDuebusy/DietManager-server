050b4df867020d4733f06171129b2841dc64a121
starkduebusy <whitesolver@gmail.com>
2019-05-03 18:05:13 +0900

    체지방률 계산해서 체성분 데이터 제공하는 기능 구현완료

--- routes/bodycomposition.js
+++ routes/bodycomposition.js
@@ -7,9 +7,35 @@ router.get('/', function(req, res, next) {
   var params = {
     userName : (req.session == undefined)? null:req.session.userName,
     targetWeight : (req.session == undefined)? null:req.session.targetWeight,
-    profileIMG : (req.session == undefined)? null:req.session.profileIMG
+    profileIMG : (req.session == undefined)? null:req.session.profileIMG,
+    recordList : []
   };
-  res.render('bodycomposition', params);
+  
+  if(req.session.session == undefined){
+    res.render('bodycomposition', params);
+  }else{
+    sqlManager(function(err, con) {
+      var checkQuery = 'SELECT BODY_WEIGHT, BODY_MUSCLE, BODY_FAT, date_format(RECORD_YMD, "%Y-%m-%d") as RECORD_YMD FROM DIET_MANAGER.BODYCOMPOSITION WHERE USER_ID = (SELECT USER_ID FROM USER WHERE EMAIL = ?);';
+      con.query(checkQuery, req.session.email, function(err, result){
+        con.release();
+        if(err){
+          con.release();
+          next(new Error('ERR006|' + req.countryCode));
+          return;
+        }
+        
+        if(result.length != 0){
+          for(var recordIndex = 0; recordIndex < result.length; recordIndex++){
+            var bodyFatPercentage = (result[recordIndex].BODY_FAT/result[recordIndex].BODY_WEIGHT)*100;
+            result[recordIndex].BODY_FAT_PERCENTAGE = Math.round((bodyFatPercentage*100))/100;
+          }
+          params.recordList = result;
+        }
+
+        res.render('bodycomposition', params);  
+      });
+    });
+  }
 });
 
 
