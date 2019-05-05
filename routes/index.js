6723f47921ac9953618ee753c5b0fa9333c7d53e
starkduebusy <whitesolver@gmail.com>
2019-05-05 18:45:12 +0900

    ffmi 기록 그래프 개발완료

--- routes/index.js
+++ routes/index.js
@@ -118,5 +118,73 @@ router.get('/bodycomposition', function(req, res, next) {
   });
 });
 
+router.get('/ffmi', function(req, res, next) {
+  if(req.session.session == undefined){
+    var resultParams = {
+      isSuccess : false,
+      needLogin : true
+    };
+    res.send(resultParams);
+    return;
+  }
+
+  sqlManager(function(err, con) {
+    var queryUser = 'SELECT * FROM USER WHERE EMAIL = ?;';
+    con.query(queryUser, req.session.email, function(err, result){
+      if(err){
+        con.release();
+        next(new Error('ERR006|' + req.countryCode));
+        return;
+      }
+
+      var resultParams = {
+        isSuccess : false,
+        ffmiList : []
+      };
+
+      for(var month = 0; month < 12; month++){
+        resultParams.ffmiList[month] = {
+          ffmi : 0
+        };
+      }
+      if(result.length != 0){
+        var height = result[0].HEIGHT/100;
+        sqlManager(function(err, con) {
+          var queryUser = 'SELECT BODY_MUSCLE, BODY_FAT, BODY_WEIGHT,  RECORD_YMD FROM DIET_MANAGER.BODYCOMPOSITION WHERE USER_ID = (SELECT USER_ID FROM DIET_MANAGER.USER WHERE EMAIL = ?) AND YEAR(RECORD_YMD) = ? ORDER BY RECORD_YMD ASC;';
+          var params = [
+            req.session.email,
+            req.query.year
+          ];
+          con.query(queryUser, params, function(err, result){
+            con.release();
+            if(err){
+              con.release();
+              next(new Error('ERR006|' + req.countryCode));
+              return;
+            }
+
+            for(var index = 0; index < result.length; index++){
+              var bodyFatPercentage = (result[index].BODY_FAT/result[index].BODY_WEIGHT);
+              var ffm = result[index].BODY_WEIGHT * (1 - (bodyFatPercentage));
+              var originFFMI = ffm / Math.pow(height,2);
+              var ffmi = ffm/Math.pow(height,2) + 6.1*(1.8 - height);
+              var month = new Date(result[index].RECORD_YMD).getMonth();
+              resultParams.ffmiList[month] = {
+                ffmi : Math.round(ffmi*1000)/1000
+              };
+            }
+            
+            resultParams.isSuccess = true;
+            res.send(resultParams);
+          });
+        });
+      }else{
+        res.send(resultParams);
+      }
+    });
+  });
+});
+
+
 
 module.exports = router;
