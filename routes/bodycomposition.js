fa9da1ecc1fbf15c071278c0f800dec8743ca2e7
min950903 <min950903@gmail.com>
2019-05-07 22:03:05 +0900

    체성분 default 값 넘겨주기

--- routes/bodycomposition.js
+++ routes/bodycomposition.js
@@ -6,16 +6,16 @@ var sqlManager = require('../manager/sqlManager');
 router.get('/', function(req, res, next) {
   var params = {
     userName : (req.session == undefined)? null:req.session.userName,
-    targetWeight : (req.session == undefined)? null:req.session.targetWeight,
+    targetWeight : (req.session == undefined)? null:req.session.targetWeight, 
     profileIMG : (req.session == undefined)? null:req.session.profileIMG,
-    recordList : []
+    recordList : []  
   };
   
   if(req.session.session == undefined){
     res.render('bodycomposition', params);
   }else{
     sqlManager(function(err, con) {
-      var checkQuery = 'SELECT BODY_WEIGHT, BODY_MUSCLE, BODY_FAT, date_format(RECORD_YMD, "%Y-%m-%d") as RECORD_YMD FROM DIET_MANAGER.BODYCOMPOSITION WHERE USER_ID = (SELECT USER_ID FROM USER WHERE EMAIL = ?);';
+      var checkQuery = 'SELECT BODY_WEIGHT, BODY_MUSCLE, BODY_FAT, date_format(RECORD_YMD, "%Y-%m-%d") as RECORD_YMD FROM DIET_MANAGER.BODYCOMPOSITION WHERE USER_ID = (SELECT USER_ID FROM USER WHERE EMAIL = ?) ORDER BY RECORD_YMD DESC ;';
       con.query(checkQuery, req.session.email, function(err, result){
         con.release();
         if(err){
@@ -30,9 +30,15 @@ router.get('/', function(req, res, next) {
             result[recordIndex].BODY_FAT_PERCENTAGE = Math.round((bodyFatPercentage*100))/100;
           }
           params.recordList = result;
+          params.defaultWeight = result[0].BODY_WEIGHT;
+          params.defaultMuscle = result[0].BODY_MUSCLE;
+          params.defaultFat = result[0].BODY_FAT;
+        }else{
+          params.defaultWeight = '',
+          params.defaultMuscle = '',
+          params.defaultFat = ''
         }
-
-        res.render('bodycomposition', params);  
+        res.render('bodycomposition', params);
       });
     });
   }
